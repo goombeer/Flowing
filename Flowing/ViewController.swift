585f8ab5252cdd1fa73994125602874c8219fc03
goombeer <highbridge71@gmail.com>
2018-02-17 21:34:26 +0900

    first commit

--- Flowing/ViewController.swift
+++ Flowing/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    
+    var displayLabel:UILabel = {
+         let label = UILabel()
+        label.text = "What usually do you do? I'm a high school student."
+        label.font = UIFont(name:"Copperplate",size:36)
+        label.frame = CGRect(x:
+            700,y:200,width:300,height:100)
+        label.sizeToFit()
+        return label
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(displayLabel)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapStart(_ sender: Any) {
+        self.startButton.isHidden = true
+        animateLabel()
+    }
+    
+    func animateLabel(){
+        UIView.animate(withDuration: 10.0, animations: {() -> Void in
+            self.displayLabel.frame = CGRect(x:-self.displayLabel.frame.width,y:200,width:self.displayLabel.frame.width,height:self.displayLabel.frame.height)
+        }, completion: {(Bool) -> Void in
+            print("終了")
+        })
+    }
 }
 
